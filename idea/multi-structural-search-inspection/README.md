1d72a8dfb2766a3e245bc009a99eb436af2b3ee7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2017-02-22 01:24:44 +0000

    Workaround for IDEA-168056

---
+++ idea/multi-structural-search-inspection/README.md
@@ -0,0 +1,3 @@
+https://youtrack.jetbrains.com/issue/IDEA-168056
+
+Only a workaround, not a repro.
---
+++ idea/multi-structural-search-inspection/src/main/java/net/twisterrob/intellij/inspection/SSBasedInspection4.java
@@ -0,0 +1,11 @@
+package net.twisterrob.intellij.inspection;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.structuralsearch.inspection.highlightTemplate.SSBasedInspection;
+
+public class SSBasedInspection4 extends SSBasedInspection {
+	@Override public @NotNull String getShortName() {
+		return "SSBasedInspection4";
+	}
+}
