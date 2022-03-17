7b18529083b9c8fa8837fd8b1251d0e4a2ba5606
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-03-17 20:39:41 +0000

    Repro for issuetracker.google.com/185418482#14

--- lint/NewApi_kotlinForEach/build.gradle.kts
+++ lint/NewApi_kotlinForEach/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
-    id("com.android.application") version "4.1.3"
-    id("org.jetbrains.kotlin.android") version "1.5.32"
+    id("com.android.application") version "4.2.2"
+    id("org.jetbrains.kotlin.android") version "1.6.10"
 }
 
 android {
     buildToolsVersion = "30.0.3"
-    compileSdkVersion(32)
+    compileSdkVersion(31)
     defaultConfig {
         applicationId = "com.repro"
         versionCode = 1
         versionName = "1.0"
 
         minSdkVersion(23)
-        targetSdkVersion(32)
+        targetSdkVersion(31)
     }
     lintOptions {
         checkOnly("NewApi")
