7324d60fe49ea7bd498219558ab4938e0da01523
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-04 16:08:36 +0100

    Repro for issuetracker.google.com/279306626

---
+++ agp/gradle82-getBuildPath-lint/README.md
@@ -0,0 +1,1 @@
+https://issuetracker.google.com/issues/279306626
---
+++ agp/gradle82-getBuildPath-lint/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+	id("com.android.library") version "8.0.1"
+}
+
+android {
+	namespace = "com.example.lib"
+	compileSdk = 31
+	defaultConfig.minSdk = 14
+}
