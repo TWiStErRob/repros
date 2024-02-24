23b3345c1788be182dbf8e4f7c54cce7d49f0d0f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-24 12:34:55 +0000

    Demo for stackoverflow.com/a/60138176/253468

---
+++ gradle/testFixtures-multiModule/README.md
@@ -0,0 +1,3 @@
+https://stackoverflow.com/a/60138176/253468
+
+Try: `gradlew modul:other:test`
---
+++ gradle/testFixtures-multiModule/modul/other/src/test/java/com/example/other/SomeTest.java
@@ -0,0 +1,8 @@
+package com.example.other;
+import com.example.Helper;
+import org.junit.Test;
+public class SomeTest {
+  @Test public void f() {
+    new Helper(); // used from :modul:one's testFixtures
+  }
+}
