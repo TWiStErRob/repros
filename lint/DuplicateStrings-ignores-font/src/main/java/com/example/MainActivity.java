7af8c473fae746269554c9cfe309bcb44ba67cb8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-03-02 16:27:22 +0000

    Repro for issuetracker.google.com/327744439

---
+++ lint/DuplicateStrings-ignores-font/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.main);
+    }
+}
